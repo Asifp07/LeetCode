351ef968fda52a7b893f49edca64b92491b05af0
Asif Pathan <asifpjscoe@gmail.com>
2026-08-12 15:05:49 +0530

    Time: 0 ms (100%), Space: 8.6 MB (80.25%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num<10) return num;
+        int sum = 0;
+        while(num>0){
+            sum += num%10;
+            num /= 10;
+            
+        }
+        if(sum>9){
+            return addDigits(sum);
+        }
+        else return sum;
+
+    }
+};
