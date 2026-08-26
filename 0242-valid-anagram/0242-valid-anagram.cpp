f4d6974b0006a8349be9c22f56bfc62bf91f6b5c
Asif Pathan <asifpjscoe@gmail.com>
2026-08-26 23:50:16 +0530

    Time: 3 ms (45.05%), Space: 9.7 MB (75.72%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()) return false;
+        map<char,int> freq1;
+        map<char,int> freq2;
+        for(auto i : s){
+            freq1[i]++;
+        }
+        for(auto i : t){
+            freq2[i]++;
+        }
+        if (freq1 == freq2) return true;
+        return false;
+      
+        
+    }
+};
