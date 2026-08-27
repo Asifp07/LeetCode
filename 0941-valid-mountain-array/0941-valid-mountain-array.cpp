d16e5e452bd241e247a3fd90cad68cc9664b1711
Asif Pathan <asifpjscoe@gmail.com>
2026-08-27 18:42:37 +0530

    Time: 20 ms (56.44%), Space: 26 MB (99.79%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.cpp
+++ 0941-valid-mountain-array/0941-valid-mountain-array.cpp
@@ -3,11 +3,11 @@ public:
     bool validMountainArray(vector<int>& arr) {
         int n = arr.size();
         int i = 0;
-        while (i+1 < n && arr[i]<arr[i+1]){
+        while (i < n-1 && arr[i]<arr[i+1]){
             i++;
         }
         if(i == n-1 || i == 0) return false;
-        while(i+1 < n && arr[i]>arr[i+1]){
+        while(i < n-1 && arr[i]>arr[i+1]){
             i++;
         }
         if (i == n-1) return true;
